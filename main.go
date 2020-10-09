ee1054768d33174e2f55f2cb41ee59f32be7361e
Daniel <danielfbm@gmail.com>
2020-10-09 22:41:03 +0800

    feat: Add a file validation

--- main.go
+++ main.go
@@ -20,6 +20,28 @@ func main() {
 	flag.Parse()
 
 	// validation
+	if !isFile(file) {
+		os.Exit(1)
+		return
+	}
+	if !isFile(patchFile) {
+		os.Exit(1)
+		return
+	}
 
-	fmt.Println("file", file, "patchFile", patchFile)
+	fmt.Println("got:", "configuration file", file, "patchFile", patchFile)
+}
+
+func isFile(file string) (ok bool) {
+	fileInfo, err := os.Stat(file)
+	if err != nil {
+		fmt.Println("file", file, " stat returned error:", err)
+		return
+	}
+	if fileInfo.IsDir() {
+		fmt.Println(file, "is a directory")
+		return
+	}
+	ok = true
+	return
 }
